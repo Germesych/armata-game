58861b730c8e93b93359fce801d9187e10efdcaf
germesych <kaiot1984@google.com>
2022-01-13 21:21:37 +0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PageIndexComponent } from './pages/page-index/page-index.component';
+import { PageNewsComponent } from './pages/page-news/page-news.component';
+import { PageMediaComponent } from './pages/page-media/page-media.component';
+import { PageCommunityComponent } from './pages/page-community/page-community.component';
+import { PageAboutTheGameComponent } from './pages/page-about-the-game/page-about-the-game.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PageIndexComponent,
+    PageNewsComponent,
+    PageMediaComponent,
+    PageCommunityComponent,
+    PageAboutTheGameComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/pages/page-about-the-game/page-about-the-game.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-page-about-the-game',
+  templateUrl: './page-about-the-game.component.html',
+  styleUrls: ['./page-about-the-game.component.scss']
+})
+export class PageAboutTheGameComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
